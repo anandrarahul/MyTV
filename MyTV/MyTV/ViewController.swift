3ac669f774ceb583fb6298bff59821b40becc7ba
Rahul Anand <rahul.anand@citrix.com>
2023-10-24 14:04:40 +0530

    Feature_01_initialProjectSetup - created xcode project - added app icon

---
+++ MyTV/MyTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTV
+//
+//  Created by Rahul Anand on 24/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
