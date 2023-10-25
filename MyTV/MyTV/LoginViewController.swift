2f951160f85e450d000aa8e2357de0975ebcb935
anandrarahul <82442422+anandrarahul@users.noreply.github.com>
2023-10-25 10:44:56 +0530

    Feature 02 UI initial setup (#2)
    * Feature_01_initialProjectSetup
    - created xcode project
    - added app icon
    
    * Feature_02_UI_InitialSetup
    - Integrated UI's
    - Added Localisations
    - made connection in View Controllers
    
    ---------
    
    Co-authored-by: Rahul Anand <rahul.anand@citrix.com>

---
+++ MyTV/MyTV/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.swift
+//  MyTV
+//
+//  Created by Rahul Anand on 24/10/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTitle: UILabel!
+    @IBOutlet weak var loginSubTitle: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var copyRightLabel: UILabel!
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        let mainTabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "mainTabBarController") as! UITabBarController
+        self.navigationController?.pushViewController(mainTabBarController, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.title = NSLocalizedString("Application.Title", comment: "app name")
+        self.loginTitle.text = NSLocalizedString("Login.Title", comment: "login")
+        self.loginSubTitle.text = NSLocalizedString("Login.SubTitle", comment: "login subtitle")
+        self.copyRightLabel.text = NSLocalizedString("CopyRight.Title", comment: "Copy right")
+        self.usernameTextField.placeholder = NSLocalizedString("Username.Title", comment: "enter username")
+        self.passwordTextField.placeholder = NSLocalizedString("Password.Title", comment: "enter password")
+    }
+
+}
+
