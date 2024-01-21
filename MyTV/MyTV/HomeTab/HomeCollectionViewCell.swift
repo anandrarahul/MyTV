6362effedf909ebfa58a7d6a3d22cbc799fc577a
Rahul Anand <rahul.anand@citrix.com>
2024-01-21 13:34:59 +0530

    Feature_05_HomeCollectionViewChanges - Added HomeCollectionViewCell.xib. - Made network Call to fetch Video Details. - Made changes for when to show the collectionView. - Handled Few error Scenarios, If some error happens during network call.

---
+++ MyTV/MyTV/HomeTab/HomeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCollectionViewCell.swift
+//  MyTV
+//
+//  Created by Rahul Anand on 21/01/24.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
