60f5df318823f78c8bb6715a8f3da889ecec74fe
anandrarahul <82442422+anandrarahul@users.noreply.github.com>
2024-01-22 10:01:25 +0530

    Feature_06_ShowVideoThumbnailAndTitle (#6)
    - Added Video Thumbnail and Title.
    - Added noImage wherever there is no image.
    - Added CollectionViewCellTests.
    
    Co-authored-by: Rahul Anand <rahul.anand@citrix.com>

--- MyTV/MyTV/HomeTab/HomeCollectionViewCell.swift
+++ MyTV/MyTV/HomeTab/HomeCollectionViewCell.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class HomeCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.imageView.image = UIImage(named: "noImage")
+    }
+    
+    func setThumbnailAndTitle(thumbnail: String?, title: String?) {
+        if let thumb = thumbnail {
+            let thumbnailUrl = URL(string: thumb)!
+            URLSession.shared.dataTask(with: thumbnailUrl) { data, response, error in
+                DispatchQueue.main.async {
+                    if let _ = error {
+                        self.imageView.image = UIImage(named: "noImage")
+                    } else {
+                        if let imageData = data {
+                            self.imageView.image = UIImage(data: imageData)
+                        }
+                    }
+                }
+            }.resume()
+        }
+        if let title = title {
+            self.titleLabel.text = title
+        }
     }
 
 }
